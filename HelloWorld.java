ec88a71771723dd75ea9c82e01e8551d392a855f
Your Name <you@example.com>
2021-10-11 11:51:50 +0700

    Update_3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Nama : Faiz Najm");
+		System.out.println("Nama : Faiz Najmuddin");
 		System.out.println("NIM  : D3RPLA4503");
 		System.out.println("Kelas: 45-03");
 	}
